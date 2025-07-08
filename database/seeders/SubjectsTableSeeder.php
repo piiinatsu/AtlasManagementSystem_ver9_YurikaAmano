7786358b10779c3657cb8a8ff98c5280bef484a2
Yurika Amano <>
2025-07-08 10:24:45 +0900

    初期データの登録

--- database/seeders/SubjectsTableSeeder.php
+++ database/seeders/SubjectsTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; // データベースの受付窓口
+use Carbon\Carbon; // 日付操作ライブラリ
 
 class SubjectsTableSeeder extends Seeder
 {
@@ -15,5 +17,19 @@ class SubjectsTableSeeder extends Seeder
     public function run()
     {
         // 国語、数学、英語を追加
+        DB::table('subjects')->insert([
+            [
+                'subject' => '国語',
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'subject' => '数学',
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'subject' => '英語',
+                'created_at' => Carbon::now(),
+            ],
+        ]);
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +17,21 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('users')->insert([
+            [
+                'over_name' => '鈴木',
+                'under_name' => '一郎',
+                'over_name_kana' => 'スズキ',
+                'under_name_kana' => 'イチロウ',
+                'mail_address' => 'ichiro@example.com',
+                'sex' => 0,
+                'birth_day' => '2001-01-01',
+                'role' => 0,
+                'password' => Hash::make('password1'),
+                'remember_token' => Str::random(10),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]
+        ]);
     }
 }
