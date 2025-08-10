516fc217bc0b7c51fffd87343fc5a48183c7e2f8
Yurika Amano <>
2025-08-10 15:20:19 +0900

    #4551 レイアウトの調整(1ログイン画面)

--- resources/views/auth/login/login.blade.php
+++ resources/views/auth/login/login.blade.php
@@ -1,29 +1,40 @@
 <x-guest-layout>
-  <form action="{{ route('loginPost') }}" method="POST">
-    <div class="w-100 vh-100 d-flex" style="align-items:center; justify-content:center;">
-      <div class="border vh-50 w-25">
-        <div class="w-75 m-auto pt-5">
-          <label class="d-block m-0" style="font-size:13px;">メールアドレス</label>
-          <div class="border-bottom border-primary w-100">
-            <input type="text" class="w-100 border-0" name="mail_address">
-          </div>
-        </div>
-        <div class="w-75 m-auto pt-5">
-          <label class="d-block m-0" style="font-size:13px;">パスワード</label>
-          <div class="border-bottom border-primary w-100">
-            <input type="password" class="w-100 border-0" name="password">
-          </div>
-        </div>
-        <div class="text-right m-3">
-          <input type="submit" class="btn btn-primary" value="ログイン">
-        </div>
-        <div class="text-center">
-          <a href="{{ route('registerView') }}">新規登録</a>
-        </div>
+  <div class="d-flex flex-column align-items-center justify-content-center min-vh-100" style="background:#EEF2F7;">
+
+    <!-- ロゴ＋フォームをまとめて中央寄せ -->
+    <div class="d-flex flex-column align-items-center">
+
+      <!-- ロゴ -->
+      <div class="login-logo d-flex justify-content-center mb-5">
+        <img src="{{ asset('image/atlas-black.png') }}" alt="Atlas Logo" class="img-fluid" style="max-width:200px; height:auto;">
       </div>
-      {{ csrf_field() }}
+
+      <!-- フォーム -->
+      <form action="{{ route('loginPost') }}" method="POST">
+        <div class="bg-white rounded shadow p-4" style="width:420px;">
+          <div class="w-75 m-auto pt-5">
+            <label class="d-block m-0" style="font-size:13px;">メールアドレス</label>
+            <div class="border-bottom border-primary w-100">
+              <input type="text" class="w-100 border-0" name="mail_address">
+            </div>
+          </div>
+          <div class="w-75 m-auto pt-5">
+            <label class="d-block m-0" style="font-size:13px;">パスワード</label>
+            <div class="border-bottom border-primary w-100">
+              <input type="password" class="w-100 border-0" name="password">
+            </div>
+          </div>
+          <div class="text-right m-3">
+            <input type="submit" class="btn btn-primary" value="ログイン">
+          </div>
+          <div class="text-center">
+            <a href="{{ route('registerView') }}">新規登録はこちら</a>
+          </div>
+        </div>
+        {{ csrf_field() }}
+      </form>
+
     </div>
-  </form>
   </div>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <script src="{{ asset('js/register.js') }}" rel="stylesheet"></script>
