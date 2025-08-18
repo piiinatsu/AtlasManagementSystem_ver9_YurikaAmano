1faed4dff152ee08e8c97208b9ef245f06431858
Yurika Amano <>
2025-08-18 23:10:34 +0900

    #4551 レイアウトの調整(カレンダータイトルの枠線修正)

--- resources/views/authenticated/calendar/general/calendar.blade.php
+++ resources/views/authenticated/calendar/general/calendar.blade.php
@@ -1,12 +1,9 @@
 <x-sidebar>
 <div class="vh-100 pt-5" style="background:#ECF1F6;">
   <div class="border w-75 m-auto pt-5 pb-5" style="border-radius:5px; background:#FFF;">
-    <div class="w-75 m-auto border" style="border-radius:5px;">
-
-      <p class="text-center">{{ $calendar->getTitle() }}</p>
-      <div class="">
-        {!! $calendar->render() !!}
-      </div>
+    <p class="text-center">{{ $calendar->getTitle() }}</p>
+    <div class="w-75 m-auto border border-top-0" style="border-radius:5px;">
+      {!! $calendar->render() !!}
     </div>
     <div class="text-right w-75 m-auto">
       <input type="submit" class="btn btn-primary" value="予約する" form="reserveParts">
